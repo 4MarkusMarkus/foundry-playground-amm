16ef4e9890edbbc15cd92d25487efad0a0176ce2
0xhoratius <horatiusone@protonmail.com>
2022-07-01 13:24:38 +0800

    m

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../utils/MockERC20.sol";
+import "./amm.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    AmmContract public ammContract;
+    MockERC20 public mockERC20;
+    function setUp() public {
+        mockERC20 = new(MockERC20("token0"));
+
+    }
 
     function testExample() public {
         assertTrue(true);
